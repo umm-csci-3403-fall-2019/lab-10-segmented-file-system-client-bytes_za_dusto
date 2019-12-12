e949181f0b99389d2f0e56f7b4ead43abf70ad70
UtkarshKr007 <kumar375@morris.umn.edu>
2019-12-12 09:16:08 -0600

    Implement adding packets and writing to files

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -12,17 +12,41 @@ public class Main{
         byte[] buf = new byte[1028];
 
         try {
+            // Establish Connection
             DatagramSocket socket = new DatagramSocket();
             InetAddress address = InetAddress.getByName("csci-4409.morris.umn.edu");
             DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
             socket.send(packet);
 
+            // A Hashmap to store files with their fileId as key
+            //  and a list of all the files in an array list
             HashMap<Byte,clientFile> files = new HashMap<>();
             ArrayList<clientFile> allFiles = new ArrayList<>();
 
             while(true) {
+                // Receive packets
                 packet = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
+
+                // capture the second index of buffer which is fileId
+                byte fileId = buf[1];
+
+                if(files.get(fileId)==null){
+                    files.put(fileId, new clientFile(packet));
+                } else {
+                    files.get(fileId).addPacket(packet);
+                }
+
+                if(files.get(fileId).isFinished()){
+                    allFiles.add(files.remove(fileId));
+                    if(files.isEmpty()){
+                        break;
+                    }
+                }
+            }
+
+            for(clientFile file: allFiles){
+                file.createFile();
             }
 
         } catch (SocketException se){
--- src/segmentedfilesystem/clientFile.java
+++ src/segmentedfilesystem/clientFile.java
@@ -1,21 +1,77 @@
 package segmentedfilesystem;
 
-import javax.xml.crypto.Data;
+import java.io.*;
 import java.net.DatagramPacket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.HashMap;
 
 
 public class clientFile {
-
-    private int packetsCounter;
+    private int maxPackets;
     private int packetsEncountered;
-    private boolean done;
-    private HashMap<Integer,byte[]> packets;
+    private boolean finished;
+    private HashMap<Integer,byte[]> packetsMap;
 
     public clientFile(DatagramPacket packet){
-        packetsCounter = Integer.MAX_VALUE;
+        maxPackets = Integer.MAX_VALUE;
         packetsEncountered = 0;
-        done = false;
-        packets = new HashMap<>();
+        finished = false;
+        packetsMap = new HashMap<>();
+
+        this.addPacket(packet);
     }
+
+    public void addPacket(DatagramPacket packet) {
+
+        packetsEncountered++;
+
+        int status = packet.getData()[0];
+        byte[] pNumberData = new byte[]{packet.getData()[2], packet.getData()[2]};
+        int packetNumber = ByteBuffer.wrap(pNumberData).getShort();
+
+
+        switch(status % 4){
+            case 3: {
+                maxPackets = packetNumber + 2;
+            }
+            case 1: {
+                packetsMap.put(packetNumber, Arrays.copyOf(packet.getData(),packet.getLength()));
+                break;
+            }
+            default:{
+                packetsMap.put(-1,Arrays.copyOf(packet.getData(),packet.getLength()));
+            }
+        }
+
+        if(packetsEncountered==maxPackets){
+            finished = true;
+        }
+    }
+
+    public void createFile() throws IOException {
+
+        byte[] headerFile = packetsMap.get(-1);
+        String fileName = new String(headerFile,2,headerFile.length-2);
+
+        File file = new File(fileName);
+        OutputStream fileOS = new FileOutputStream(file);
+        System.out.println(fileName);
+
+        if(file.createNewFile()){
+            System.out.println("File with same name already exists");
+        }
+
+        for(int i=0; i <maxPackets - 1; i++){
+            byte[] packet = packetsMap.get(i);
+            fileOS.write(packet,4,packet.length-4);
+        }
+    }
+
+
+    public boolean isFinished(){
+        return finished;
+    }
+
+
 }
